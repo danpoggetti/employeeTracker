041767c03a1dfcf3490a66e65ffc46c79f1b3f56
danpoggetti <104542385+danpoggetti@users.noreply.github.com>
2022-11-29 17:23:17 -0800

    added mysql and require

---
+++ employee-tracker/index.js
@@ -0,0 +1,10 @@
+const mysql = require('mysql2');
+const inquirer = require('inquirer');
+
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    database: "employeeTracker_db",
+    password: "athletics12",
+    user: "root"
+})
